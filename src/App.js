43a1278de3eb05bf15213a1acfc112e41937e24f
tejavallala <tejavallala@gmail.com>
2025-03-03 15:09:29 +0530

    Enhanced UI & new features

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NewUserForm from './Components/Newuserform';
-import Nav from './Components/Nav';
-import UserForm from './Components/UserForm';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
-import About from './Components/About';
-import Services from './Components/Services';
-import Dashboard from './Components/UserDashboard';
-import Admin from './Components/Admin';
-import AdminDashboard from './Components/AdminDashboard';
-import CourseAddForm from './Components/CourseAddForm';
-import CourseList from './Components/CourseList';
-import EditCourse from './Components/EditCourse';
-import ViewCourse from './Components/ViewCourse';
-import MyCourses from './Components/MyCourse';
-import PaymentPage from './Components/Payment';
-import PaidCourse from './Components/PaidCourse';
-import CourseContent from './Components/CourseContent';
-import Test from './Components/Test';
-import Video from './Components/Video';
-import SupportForm from './Components/SupportForm';
-import UserProblem from './Components/UserProblems';
-import Answer from './Components/Answer';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import NewUserForm from "./Components/Users/UserRegistration";
+import Nav from "./Components/Dashboard/MainDashboard";
+import UserForm from "./Components/Users/UserLogin";
+import About from "./Components/Items/About";
+import Services from "./Components/Items/Services";
+import Dashboard from "./Components/Dashboard/UserDashboard";
+import Admin from "./Components/Instructor/Admin";
+import AdminDashboard from "./Components/Dashboard/AdminDashboard";
+import CourseAddForm from "./Components/Courses/CourseAddForm";
+import CourseList from "./Components/Courses/CourseList";
+import ViewCourse from "./Components/Courses/ViewCourse";
+import Cart from "./Components/Courses/Cart";
+import PaymentPage from "./Components/Payment/Payment";
+import PaidCourse from "./Components/Courses/PaidCourse";
+import CourseContent from "./Components/Courses/CourseContent";
+import SupportForm from "./Components/Support/SupportForm";
+import UserProfile from "./Components/Profiles/UserProfile"; 
+import InstructorRegistration from "./Components/Instructor/InstructorRegistration"; 
+import AdminProfile from "./Components/Profiles/AdminProfile";
+import PaymentSuccess from "./Components/Payment/PaymentSuccess";
+import UserData from "./Components/Users/UserData";
+import CourseContentManager from "./Components/Courses/CourseContentManager";
 
 function App() {
   return (
@@ -37,24 +37,26 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/AdminDashboard" element={<AdminDashboard />} />
-          <Route path="/admin" element={<Admin/>}/>
-          <Route path="/dashboard" element={<Dashboard/>}/>
-          <Route path="/add-course" element={<CourseAddForm/>}/>
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/add-course" element={<CourseAddForm />} />
           <Route path="/course-list" element={<CourseList />} />
-          <Route path="/edit-course/:id" element={<EditCourse />} />
           <Route path="view-courses" element={<ViewCourse />} />
-          <Route path="my-courses" element={<MyCourses/>} />
-          <Route path="/payment" element={<PaymentPage/>} />
-          <Route path="/paid-course" element={<PaidCourse/>}/>
-          <Route path="/course-content" element={<CourseContent/>}/>
-          <Route path="/Test" element={<Test/>}/>
-          <Route path ="/Video" element={<Video/>}/>
-          <Route path ="/support-form" element={<SupportForm/>}/>
-          <Route path='/user-problems' element={<UserProblem/>}/>
-          <Route path='Answer' element={<Answer/>}/>
-          
-
-
+          <Route path="add-to-cart" element={<Cart />} />
+          <Route path="/payments" element={<PaymentPage />} />
+          <Route path="/my-courses" element={<PaidCourse />} />
+          <Route path="/course-content" element={<CourseContent />} />
+          <Route path="/support" element={<SupportForm />} />
+          <Route path="/profile" element={<UserProfile />} />
+          <Route path="/payment-success" element={<PaymentSuccess />} />
+          <Route
+            path="/instructor-registration"
+            element={<InstructorRegistration />}
+          />
+          <Route path="admin-profile" element={<AdminProfile />} />
+          <Route path="/user-data" element={<UserData />} />
+          <Route path="/manage-content" element={<CourseContentManager />} />
+          <Route path="/course-content/:courseId" element={<CourseContent />} />
         </Routes>
       </div>
     </Router>
